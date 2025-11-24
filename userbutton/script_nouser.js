9d8746d17bd757bcb947db802ebf51eaeaaf1324
Darius <dariusagapi6@gmail.com>
2025-11-24 13:37:46 +0200

    Logout button fixed from tickets, removed user text from support, fix tickets show in navbar for all users

--- userbutton/script_nouser.js
+++ userbutton/script_nouser.js
@@ -3,13 +3,13 @@ const userNameDisplay = document.getElementById("userName");
 const username = localStorage.getItem("user");
 
 if (!username) {
-  userNameDisplay.textContent = "Guest";
+  userNameDisplay.textContent = "";
   logoutBtn.textContent = "Login";
   logoutBtn.addEventListener("click", () => {
     window.location.href = "login/login.html";
   });
 } else {
-  userNameDisplay.textContent = username;
+  userNameDisplay.textContent = "";
   logoutBtn.textContent = "Logout";
 
   logoutBtn.addEventListener("click", async () => {
